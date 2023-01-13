c838c0c6beee13a695833c25fee676c3d51dd3d0
Deep Roop <deepsinghroop@gmail.com>
2023-01-13 16:40:33 +0530

    cart is complete

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -3,7 +3,22 @@ import { CartContext } from "../CartContext";
 import { useContext } from "react";
 import { getProductData } from "../productsStore";
 
-function CartProduct(props) {}
+function CartProduct(props) {
+    const cart = useContext(CartContext);
+    const id = props.id;
+    const quantity = props.quantity;
+    const productData = getProductData(id);
+
+    return (
+        <>
+            <h3>{productData.title}</h3>
+            <p>{quantity}</p>
+            <p>${ (quantity * productData.price).toFixed(2) }</p>
+            <Button size="sm" onClick={() => cart.deleteFromCart(id)}>Remove</Button>
+            <hr></hr>
+        </>
+    )
+}
 
 
 export default CartProduct;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import {Button, Container, Navbar, Modal} from 'react-bootstrap';
 import { useState, useContext } from 'react';
 import { CartContext } from '../CartContext';
+import CartProduct from './CartProduct';
 
 function NavbarComponent() {
     const cart = useContext(CartContext);
@@ -30,7 +31,8 @@ function NavbarComponent() {
                       <>
                         <p>Items in your cart: </p>
                         {cart.items.map((currentProduct, idx) => (
-                            <h1>{currentProduct.id}</h1>
+                            
+                            <CartProduct key={idx} id={currentProduct.id} quantity={currentProduct.quantity}></CartProduct>
                         ))}
 
                         <h1>Total Cost: {cart.getTotalCost().toFixed(2)}</h1>
